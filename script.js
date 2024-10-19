5e9edacb3ae2d0c06a60cab7e0b96fa682ebdbd2
workJpiyush <PIYUSHJWORK@GMAIL.COM>
2024-10-20 02:52:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example JavaScript to show alerts for demo purposes.
+console.log("Welcome to the Online Art Store!");
+
+document.addEventListener("DOMContentLoaded", () => {
+    alert("Welcome! Explore art and grab amazing deals!");
+});
